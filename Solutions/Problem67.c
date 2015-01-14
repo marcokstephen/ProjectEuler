a6a2f68aeb613b9b951be7807fe2a9e474c0c35e
Stephen Marcok <marcok.stephen@gmail.com>
2015-01-14 16:26:31 -0500

    Update Problem67.c

--- Solutions/Problem67.c
+++ Solutions/Problem67.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//num of rows, user must set
+//num of rows, user must set (because this code is used for the Problem 18 version as well)
 int numRows = 100;
 
 int arraylen = 0; //gets changed in main
@@ -11,11 +11,14 @@ struct node{
    int maxbelow;
 };
 
+//find max of two numbers
 int max(int a, int b){
    if (a > b) return a;
    return b;
 }
 
+//reads in the data from the triangle.txt file provided by ProjectEuler
+//creates nodes for each number in the triangle
 struct node** add_data(void){
    FILE* fp = fopen("triangle.txt","r");
    int i = 0;
@@ -35,6 +38,7 @@ struct node** add_data(void){
    return nodeArray;
 }
 
+//destroying the nodes and freeing the memory
 void destroy_data(struct node** array){
    for (int i = 0; i < arraylen; i++){
       free(array[i]);
@@ -42,6 +46,7 @@ void destroy_data(struct node** array){
    free(array);
 }
 
+//find the current row of the triangle depending on the array's index
 int findCurrentRow(int i){
    int input = i + 1;
    int test = 0;
@@ -53,8 +58,9 @@ int findCurrentRow(int i){
    return test-1;
 }
 
+//finding the value of the maximum path
 void find_maxbelow(struct node** array){
-   for (int i = arraylen - 1; i >= 0; i--){
+   for (int i = arraylen - 1; i >= 0; i--){ //go backwards and build from the bottom of the triangle to the top
       int currentRow = findCurrentRow(i);
       if (currentRow == numRows){
          array[i]->maxbelow = array[i]->key;
